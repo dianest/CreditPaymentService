8c489765e4c581f39fcd1ca1f1ede60cbff02f41
dianest <nddgdd@gmail.com>
2020-08-31 15:11:10 +0100

    Change persentage

--- src/com/netology/hw/Main.java
+++ src/com/netology/hw/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         CreditCalculatorService service = new CreditCalculatorService();
-        double payment1 = service.calculate(1_000_000,1, 9.99);
+        double payment1 = service.calculate(1_000_000,1, 0.0999);
         System.out.println(payment1);
 
-        double payment2 = service.calculate(1_000_000,2, 9.99);
+        double payment2 = service.calculate(1_000_000,2, 0.0999);
         System.out.println(payment2);
 
-        double payment3 = service.calculate(1_000_000,3, 9.99);
+        double payment3 = service.calculate(1_000_000,3, 0.0999);
         System.out.println(payment3);
     }
 }
